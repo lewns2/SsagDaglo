39d91037e425e37ba6025404f21493f34f368aac
unknown <lewns2002@naver.com>
2022-10-18 17:29:38 +0900

    [add] MySQL setting, Swagger setting, User Entity, User Repo

---
+++ Backend/src/main/java/com/nds/ssagdaglo/api/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.nds.ssagdaglo.api.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+}
---
+++ Backend/src/main/java/com/nds/ssagdaglo/api/service/UserService.java
@@ -0,0 +1,7 @@
+package com.nds.ssagdaglo.api.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
