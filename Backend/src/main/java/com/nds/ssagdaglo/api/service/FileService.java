7460009a49814b2f614e023bcc4b48156ac98196
lewns2 <lewns2002@naver.com>
2022-11-01 00:02:29 +0900

    [temp] put object to aws s3

--- Backend/src/main/java/com/nds/ssagdaglo/api/service/FileService.java
+++ Backend/src/main/java/com/nds/ssagdaglo/api/service/FileService.java
@@ -13,12 +13,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.transfer.TransferManager;
-import com.amazonaws.services.s3.transfer.Upload;
 
 import java.io.File;
 import java.io.IOException;
@@ -34,12 +35,44 @@ public class FileService {
     @Value("${file.dir}")
     private String fileDir;
 
-//    @Value("${cloud.aws.s3.bucket}")
-//    private String bucket;
-//    private final AmazonS3Client amazonS3Client;
-
     private final FileRepository fileRepository;
     private final UserRepository userRepository;
+
+    // S3 업로드 함수
+    public static void uploadObject(String[] args) throws IOException {
+        Regions clientRegion = Regions.DEFAULT_REGION;
+        String bucketName = "sdgl-files-bucket";
+        String stringObjKeyName = "input";
+        String fileObjKeyName = "inputTest";
+        String fileName = "test";
+
+        try {
+            //This code expects that you have AWS credentials set up per:
+            // https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/setup-credentials.html
+            AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
+                    .withRegion(Regions.AP_NORTHEAST_2)
+                    .build();
+
+            // Upload a text string as a new object.
+            s3Client.putObject(bucketName, stringObjKeyName, "Uploaded String Object");
+
+            // Upload a file as a new object with ContentType and title specified.
+            PutObjectRequest request = new PutObjectRequest(bucketName, fileObjKeyName, new File(fileName));
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("plain/text");
+            metadata.addUserMetadata("title", "someTitle");
+            request.setMetadata(metadata);
+            s3Client.putObject(request);
+        } catch (AmazonServiceException e) {
+            // The call was transmitted successfully, but Amazon S3 couldn't process
+            // it, so it returned an error response.
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 couldn't be contacted for a response, or the client
+            // couldn't parse the response from Amazon S3.
+            e.printStackTrace();
+        }
+    }
 
     // 업로드된 파일을 저장하는 함수
     @Transactional(rollbackFor = Exception.class)
@@ -62,26 +95,16 @@ public class FileService {
                 .originPath(savedPath)
                 .filename(originName)
                 .user(userRepository.findByUserNickName(userNickName).get())
-//                .createdDate(LocalDate.now())
-//                .updateDate(LocalDate.now())
                 .build();
 
         file.transferTo(new java.io.File(savedPath));
 
-        // S3에 업로드
-//        final TransferManager transferManager = new TransferManager(this.amazonS3Client);
-        // 요청 객체 생성
-//        final PutObjectRequest request = new PutObjectRequest(bucket, savedName, file);
-        // 업로드 시도
-//        final Upload upload = transferManager.upload(request);
-
-        //
         FileEntity savedFileEntity = fileRepository.save(fileEntity);
 
         return savedFileEntity.getFileNo();
     }
 
-    // 사용자의 파일 목록을 조회하는 함수
+    // 특정 사용자의 파일 목록을 조회하는 함수
     public List<List> getUserFileList(String userNickName, Pageable pageable) {
         List<List> data = new ArrayList<>();
 
